0813fc3e65d55f1701664de9f0e60154b0f4b69e
psclaudia <claaudiia12@gmail.com>
2018-07-13 12:59:13 +0200

    pokemon

--- curso/Jquery/pokemon/sw.js
+++ curso/Jquery/pokemon/sw.js
@@ -1,13 +1,55 @@
+var id=1;
+var i;
+var x = "";
+var j;
+var w = "";
 $().ready(function(){
-	var id=1;
-	$.get("40.118.8.76/pokemons/" + id ,
-		function(data){
-			$("#code").text(data.name);
-			$("#normal").text(data.films[1]);
-			$("#shiny").text(data.films[1]);
-			
-		}, "json")	
-		.always(function() {
-			alert($("#name").text());
-		});
+	$("#prev").prop("disabled",true);
+	getinfo();
+	$("#next").click(function() {
+		if(id +1  == 6) $("#next").prop("disabled",true);
+		else $("#prev").prop("disabled",false);
+		++id;
+		getinfo();
+	});
+
+	$("#prev").click(function() {
+		if(id- 1 == 1) $("#prev").prop("disabled",true);
+		else $("#next").prop("disabled",false);
+		--id;
+		getinfo();
+	});
+	$("#search").click(function() {
+		search();
+	});		
 });
+function getinfo() {
+	$.get("http://40.118.8.76/pokemons/" + id ,
+		function(data){
+			clear();
+			$("#code").text(data.num);
+			$("#normal").append("<img src="+data.images[0].front+" >");
+			$("#shiny").append("<img src="+data.images[1].front_shiny+" >");
+			$("#name").text(data.name);
+			for(i = 0; i < data.types.length; i++) {
+				x += data.types[i].type.name + "<br>";
+			}
+			$("#type").html(x);
+			for(j in data.moves.HM) {
+					w += data.moves.HM[j] + "<br>";
+				}
+			if(w == "") $("#hm").html("none");
+			else $("#hm").html(w);
+		}, "json");
+}
+
+function clear() {
+	x = "";
+	w = "";
+	$("#code").text("");
+	$("#normal").text("");
+	$("#shiny").text("");
+	$("#name").text("");
+}
+
+		
