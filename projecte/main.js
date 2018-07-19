386c766fe7cb5915a59060d2681b7b125135250d
psclaudia <claaudiia12@gmail.com>
2018-07-19 22:54:54 +0200

    project

--- projecte/main.js
+++ projecte/main.js
@@ -1,54 +1,57 @@
-var int;
-window.onload = function() {
-	/*function start();*/
-	int = setInterval(imagen, 1000);
-	 /*document.getElementById("izq").onclick = function() {
-	 	clearInterval(int);
-	 	izquierda();
-	 	int = setInterval(imagen,2000);
-	 }*/
-}
-var contador = 1;
-function imagen() {
-	if (contador == 1) {
-		document.getElementById("fotos").style.backgroundImage = "url('media/got.jpg')";
-	}
-	else if(contador == 2) {
-		document.getElementById("fotos").style.backgroundImage = "url('media/criada.png')";
-	}
-	else if(contador == 3) {
-		document.getElementById("fotos").style.backgroundImage = "url('media/westworld.jpg')";
-	}
-	else if(contador == 4) {
-		document.getElementById("fotos").style.backgroundImage = "url('media/reasons.jpg')";
-	}
-	else if(contador == 5) {
-		document.getElementById("fotos").style.backgroundImage = "url('media/suits.jpg')";
-	}
-	else if(contador == 6) {
-		document.getElementById("fotos").style.backgroundImage = "url('media/vikingos.jpg')";
-	}
-	else if(contador == 7) {
-		document.getElementById("fotos").style.backgroundImage = "url('media/blackmirror.jpg')";
-	}
-	else if(contador == 8) {
-		document.getElementById("fotos").style.backgroundImage = "url('media/breakingbad.jpg')";
-	}
-	else if(contador == 9) {
-		document.getElementById("fotos").style.backgroundImage = "url('media/orange.jpg')";
-	}
-	else if(contador == 10) {
-		document.getElementById("fotos").style.backgroundImage = "url('media/blacklist.jpg')";
-	}
-	else if(contador == 11) {
-		document.getElementById("fotos").style.backgroundImage = "url('media/outlander.jpg')";
-	}
-	else if(contador == 12) {
-		document.getElementById("fotos").style.backgroundImage = "url('media/bones.jpg')";
-	}
-	else if(contador == 13) {
-		document.getElementById("fotos").style.backgroundImage = "url('media/sherlock.jpg')";
-		contador = 0;
-	}
-	++contador;
-}
+var numserie = "0";
+var num = 0;
+
+$(document).ready(function() {
+	$(".description").css("display","none");
+
+	setInterval(carrusel,3000);
+ 	
+ 	function carrusel() {
+ 		$(".series img").removeClass("opaque");
+ 		if (num == 9) num = 0;
+ 		else ++num;
+ 		numserie = num.toString();
+ 		$(".series img").eq(numserie).addClass("opaque");
+	    $("#controls span").removeClass("selected");
+	    $(this).addClass("selected");
+ 	}
+
+ 	$("#controls").on('click', 'span', function() {
+	   	$(".series img").removeClass("opaque");
+	    var newImage = $(this).index();
+	    console.log(newImage);
+	    $(".series img").eq(newImage).addClass("opaque");
+	    $("#controls span").removeClass("selected");
+	    $(this).addClass("selected");
+	    var aux = $(this).attr("id");
+	    numserie = aux.charAt(aux.length-1);
+    }),
+    /*funcion popup*/
+    "use strict";
+    $(".series").click(function () {
+        $(".series").fadeOut();
+        $("#controls").fadeOut();
+        $(".show").fadeIn();
+        console.log(numserie);
+        $(".seriedescripcion"+numserie).show();
+        console.log(numserie);
+    }),
+    
+    $("span, .overlay").click(function () {
+        $(".show").fadeOut();
+         $(".series").fadeIn();
+        $("#controls").fadeIn();
+    }),
+
+    $("#controls span").click(function () {
+    	var id = $(this).attr('id');
+    	console.log(id);
+    	numserie =id.charAt(id.length-1);
+    	console.log(numserie);
+    	$(".description").css("display","none");
+    	$(".seriedescripcion" + numserie).css("display","block");
+    });
+
+
+
+  });
