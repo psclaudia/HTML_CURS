82f52f17dd32126eb6e0d18bf82890e8a34f5116
psclaudia <claaudiia12@gmail.com>
2018-07-21 13:40:35 +0200

    project

--- projecte/main.js
+++ projecte/main.js
@@ -4,11 +4,26 @@ var num = 0;
 $(document).ready(function() {
 	$(".description").css("display","none");
 
+	/*$("#logo").on('click', 'span', function() {
+		numserie = Math.floor(Math.random()*10);
+		console.log(numserie);
+        $(".series").click();
+        console.log(numserie); la random es esta yo cambiaría la forma de hacer el random
+    });*/
+
+    $("#logo").on('click', 'span', function() {
+		num = Math.floor(Math.random()*10);
+		numserie = num.toString();
+		console.log(numserie);
+        $("#serietext" + numserie).click(); //no va...
+        setTimeout(function(){ $(".series").click(); }, 2000);
+        console.log(numserie);
+    });
+
  	$("#controls").on('click', 'span', function() {
  		$("video")[numserie].currentTime = 0;
 	   	$(".series img").removeClass("opaque");
 	    var newImage = $(this).index();
-	    console.log(newImage);
 	    $(".series img").eq(newImage).addClass("opaque");
 	    $("#controls span").removeClass("selected");
 	    $(this).addClass("selected");
@@ -18,13 +33,12 @@ $(document).ready(function() {
     /*funcion popup*/
     "use strict";
     $(".series").click(function () {
+    	console.log(numserie);
     	$("video")[numserie].currentTime = 0;
         $(".series").fadeOut();
         $("#controls").fadeOut();
         $(".show").fadeIn();
-        console.log(numserie);
         $(".seriedescripcion"+numserie).show();
-        console.log(numserie);
     }),
     
     $("span, .overlay").click(function () {
@@ -37,9 +51,7 @@ $(document).ready(function() {
 
     $("#controls span").click(function () {
     	var id = $(this).attr('id');
-    	console.log(id);
     	numserie =id.charAt(id.length-1);
-    	console.log(numserie);
     	$(".description").css("display","none");
     	$(".seriedescripcion" + numserie).css("display","block");
     });
